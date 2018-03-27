c1b32ecc4a3c5666032cb44511d1980c05f8113e
Natasha <nschimka@yahoo.com>
2018-03-27 17:15:13 -0500

    Implement advanced login with optional 'remember me'

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -34,10 +34,28 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
+
+    #Simulate a user clicking logout in a second browser
+    delete logout_path
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path, count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
+  end
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+
+    #the virtual remember_token attribute can't be accessed in the integration
+    #test, so that's why you make user an instance variable (@user) in the
+    #sessions controller and access it with assigns(:user)
+    assert_equal cookies['remember_token'], assigns(:user).remember_token
+  end
+
+  test "login without remembering" do
+    log_in_as(@user, remember_me: '1')
+    log_in_as(@user, remember_me: '0')
+    assert_empty cookies['remember_token']
   end
 
 end
