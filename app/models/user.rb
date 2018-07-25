69750c99c60e0057da866cab8cf3418d4ea8277c
Natasha <nschimka@yahoo.com>
2018-07-25 18:13:49 -0500

    Add password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
 	has_secure_password
 	validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
-	attr_accessor :remember_token, :activation_token
+	attr_accessor :remember_token, :activation_token, :reset_token
 
 	def remember
 		self.remember_token = User.new_token
@@ -45,6 +45,22 @@ class User < ApplicationRecord
 
 	def send_activation_email
 		UserMailer.account_activation(self).deliver_now
+	end
+
+	def create_reset_digest
+		self.reset_token = User.new_token
+		update_attributes(reset_digest: User.digest(reset_token),
+						reset_sent_at: Time.zone.now)
+		#update_attribute(:reset_digest, User.digest(reset_token))
+		#update_attribute(:reset_sent_at, Time.zone.now)
+	end
+
+	def send_password_reset_email
+		UserMailer.password_reset(self).deliver_now
+	end
+
+	def password_reset_expired?
+		reset_sent_at < 2.hours.ago # < is earlier than, not less than
 	end
 
 	private
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   get '/signup', to: 'users#new'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
@@ -8,6 +12,8 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 end
